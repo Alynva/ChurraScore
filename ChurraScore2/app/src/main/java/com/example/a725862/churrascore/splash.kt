7bc6c4c915c87964a559dd97d9f40079c96ff5a2
Alisson Nunes <alynva@gmail.com>
2018-10-17 15:10:14 -0300

    Added Splash Screen with Glide

---
+++ ChurraScore2/app/src/main/java/com/example/a725862/churrascore/splash.kt
@@ -0,0 +1,27 @@
+package com.example.a725862.churrascore
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class splash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Glide.with(this)
+                .load("https://abrilvip.files.wordpress.com/2018/07/dsc05420-e1531944649584.jpg")
+                .placeholder(R.mipmap.splash_bg)
+                .into(splash_img);
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
